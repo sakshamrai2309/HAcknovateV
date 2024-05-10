c536a7d42025ff632edc8fc1b659d5c8dbbac7fa
Ritik sharma <98308171+Emritik@users.noreply.github.com>
2024-05-10 22:03:01 +0530

    Add files via upload

--- Guiding_light/views.py
+++ Guiding_light/views.py
@@ -3,3 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def contact(request):
+    return render(request,'contact.html')
+def appointment(request):
+    return render(request,'appointment.html')
+
+def features(request):
+    return render(request,'features.html')
+
+def testimonial(request):
+    return render(request,'testimonial.html')
